7348f7bba801b66fcba99c1d90f9041a1b784dd2
aqibcs <csaqibshah@gmail.com>
2025-04-26 18:01:42 +0100

    Customize admin interface for models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Skill, Education, Experience, Contact
 
-# Register your models here.
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ('name', 'proficiency')
+    search_fields = ('name',)
+
+@admin.register(Education)
+class EducationAdmin(admin.ModelAdmin):
+    list_display = ('institution', 'degree', 'field_of_study', 'start_date', 'end_date')
+    search_fields = ('institution', 'degree')
+
+@admin.register(Experience)
+class ExperienceAdmin(admin.ModelAdmin):
+    list_display = ('company', 'position', 'start_date', 'end_date', 'current')
+    search_fields = ('company', 'position')
+    list_filter = ('current',)
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'subject', 'created_at')
+    search_fields = ('name', 'email', 'subject')
+    readonly_fields = ('created_at',)
